b20cd3ba89f5c0f9d143d6b835193f9e90b1f179
Alaina Hardie <alaina@precisionnutrition.com>
2016-05-28 20:28:07 -0400

    Convert To Ember Data

--- app/controllers/political-compass.js
+++ app/controllers/political-compass.js
@@ -1,7 +1,23 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+const { computed, Controller } = Ember;
+
+export default Controller.extend({
   chartMode: 'Chart',
+
+  filteredSeries: computed.filterBy('model', 'display', true),
+
+  chartData: computed('filteredSeries.@each.display', function () {
+    let series = this.get('filteredSeries');
+
+    return series.map(function(item) {
+      return {
+        name: item.get('name'),
+        color: item.get('color'),
+        data: item.get('values'),
+      };
+    });
+  }),
 
   chartOptions: {
     chart: {
@@ -62,89 +78,29 @@ export default Ember.Controller.extend({
       borderWidth: 1,
       borderRadius: 3,
     },
-  },
-
-  plotOptions: {
-    scatter: {
-      marker: {
-        radius: 7,
+    plotOptions: {
+      scatter: {
+        marker: {
+          radius: 7,
+          states: {
+            hover: {
+              enabled: true,
+              lineColor: 'rgb(100,100,100)'
+            }
+          }
+        },
         states: {
           hover: {
-            enabled: true,
-            lineColor: 'rgb(100,100,100)'
+            marker: {
+              enabled: false
+            }
           }
+        },
+        tooltip: {
+          headerFormat: '<b>{series.name}</b><br>',
+          pointFormat: '{point.x} Left/right, {point.y} Authoritarian/Libertarian'
         }
-      },
-      states: {
-        hover: {
-          marker: {
-            enabled: false
-          }
-        }
-      },
-      tooltip: {
-        headerFormat: '<b>{series.name}</b><br>',
-        pointFormat: '{point.x} Left/right, {point.y} Authoritarian/Libertarian'
       }
-    }
+    },
   },
-
-  series: [{
-    name: 'Cdn Lib (2015)',
-    color: 'rgba(255, 0, 0, 0.5)',
-    data: [[2, 4]]
-
-  }, {
-    name: 'Cdn Con (2015)',
-    color: 'rgba(119, 152, 191, 0.5)',
-    data: [[8.5,7]]
-  }, {
-    name: 'Cdn NDP (2015)',
-    color: 'rgba(255, 102, 0, 0.5)',
-    data: [[-0.3, -1.5]]
-  }, {
-    name: 'Alaina (2016)',
-    color: 'rgba(255, 105, 180, 1)',
-    data: [[-5.38, -7.23]]
-  }, {
-    name: 'Krista (2016)',
-    color: 'rgba(148,0,211, 1)',
-    data: [[-6.5, -6.77]]
-  }, {
-    name: 'Cynthia (2016)',
-    color: 'rgba(34,139,34, 1)',
-    data: [[-4.2, -6.5]]
-  }, {
-    name: 'Trump (2016)',
-    color: 'rgba(119, 152, 191, 1)',
-    data: [[8,9]]
-  }, {
-    name: 'Nader (2008)',
-    color: 'rgba(0, 191, 0, 1)',
-    data: [[-4.75,-2.9]]
-  }, {
-    name: 'Obama (2008)',
-    color: 'rgba(0, 0, 191, 1)',
-    data: [[2.8,2]]
-  }, {
-    name: 'Obama (2012)',
-    color: 'rgba(0, 0, 191, 1)',
-    data: [[6,6]]
-  }, {
-    name: 'Romney (2012)',
-    color: 'rgba(220,20,60, 1)',
-    data: [[6.5,7]]
-  }, {
-    name: 'Clinton (2008)',
-    color: 'rgba(30,144,255, 1)',
-    data: [[4,2]]
-  }, {
-    name: 'Clinton (2016)',
-    color: 'rgba(30,144,255, 1)',
-    data: [[7,4]]
-  }, {
-    name: 'Sanders (2016)',
-    color: '  rgba(0,191,255,1)',
-    data: [[-2, 0]]
-  }]
 });
---
+++ app/models/person.js
@@ -0,0 +1,130 @@
+import Model from 'ember-data/model';
+import attr from 'ember-data/attr';
+
+var Person = Model.extend({
+  name: attr('string'),
+  color: attr('string'),
+  values: attr('array'),
+  display: attr('boolean'),
+  roles: attr('array'),
+});
+
+Person.reopenClass({
+  FIXTURES: [
+  {
+    id:1,
+    name: 'Cdn Lib (2015)',
+    color: 'rgba(255, 0, 0, 0.5)',
+    values: [[2, 4]],
+    display: true,
+    role: 'canadian'
+  }, {
+    id:2,
+    name: 'Cdn Con (2015)',
+    color: 'rgba(119, 152, 191, 0.5)',
+    values: [[8.5,7]],
+    display: true,
+    role: 'canadian'
+  }, {
+    id:3,
+    name: 'Cdn NDP (2015)',
+    color: 'rgba(255, 102, 0, 0.5)',
+    values: [[-0.3, -1.5]],
+    display: true,
+    role: 'canadian'
+  }, {
+    id:4,
+    name: 'Lachlan (2016)',
+    color: 'rgba(255, 105, 0, 1)',
+    values: [[-5.38, -6.1]],
+    display: true,
+    role: 'friends'
+  }, {
+    id:5,
+    name: 'Alastair (2016)',
+    color: 'rgba(255, 0, 180, 1)',
+    values: [[-7.5, -7.59]],
+    display: true,
+    role: 'friends'
+  }, {
+    id:6,
+    name: 'Alaina (2016)',
+    color: 'rgba(255, 105, 180, 1)',
+    values: [[-5.38, -7.23]],
+    display: true,
+    role: 'friends'
+  }, {
+    id:7,
+    name: 'Krista (2016)',
+    color: 'rgba(148,0,211, 1)',
+    values: [[-6.5, -6.77]],
+    display: true,
+    role: 'friends'
+  }, {
+    id:8,
+    name: 'Cynthia (2016)',
+    color: 'rgba(34,139,34, 1)',
+    values: [[-4.2, -6.5]],
+    display: true,
+    role: 'friends'
+  }, {
+    id:9,
+    name: 'Trump (2016)',
+    color: 'rgba(119, 152, 191, 1)',
+    values: [[8,9]],
+    display: true,
+    role: 'republican'
+  }, {
+    id:10,
+    name: 'Nader (2008)',
+    color: 'rgba(0, 191, 0, 1)',
+    values: [[-4.75,-2.9]],
+    display: true,
+    role: 'us'
+  }, {
+    id:11,
+    name: 'Obama (2008)',
+    color: 'rgba(0, 0, 191, 1)',
+    values: [[2.8,2]],
+    display: true,
+    role: 'democrat'
+  }, {
+    id:12,
+    name: 'Obama (2012)',
+    color: 'rgba(0, 0, 191, 1)',
+    values: [[6,6]],
+    display: true,
+    role: 'democrat'
+  }, {
+    id:13,
+    name: 'Romney (2012)',
+    color: 'rgba(220,20,60, 1)',
+    values: [[6.5,7]],
+    display: true,
+    role: 'republican'
+  }, {
+    id:14,
+    name: 'Clinton (2008)',
+    color: 'rgba(30,144,255, 1)',
+    values: [[4,2]],
+    display: true,
+    role: 'democrat'
+  }, {
+    id:15,
+    name: 'Clinton (2016)',
+    color: 'rgba(30,144,255, 1)',
+    values: [[7,4]],
+    display: true,
+    role: 'democrat'
+  }, {
+    id:16,
+    name: 'Sanders (2016)',
+    color: '  rgba(0,191,255,1)',
+    values: [[-2, 0]],
+    display: true,
+    role: 'democrat'
+  }
+  ]
+});
+
+export default Person;
